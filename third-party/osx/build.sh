67c90b676200ce860ed0bb1b51c0fe5cca6369ac
Jonathon Fowler <jf@jonof.id.au>
2017-02-02 22:47:34 +1000

    use xcrun to invoke more tools

--- third-party/osx/build.sh
+++ third-party/osx/build.sh
@@ -3,6 +3,8 @@
 oggurl=http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz
 vorbisurl=http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.5.tar.gz
 
+export CFLAGS="-arch i386 -arch x86_64 -mmacosx-version-min=10.8"
+
 export MAKE="xcrun make"
 export CC="xcrun clang"
 export CXX="xcrun clang++"
@@ -10,7 +12,11 @@ export LD="xcrun ld"
 export AR="xcrun ar"
 export RANLIB="xcrun ranlib"
 export STRIP="xcrun strip"
-export CFLAGS="-arch i386 -arch x86_64 -mmacosx-version-min=10.8"
+export OTOOL="xcrun otool"
+export LIPO="xcrun lipo"
+export NM="xcrun nm"
+export NMEDIT="xcrun nmedit"
+export DSYMUTIL="xcrun dsymutil"
 
 check_tools() {
     echo "+++ Checking build tools"
